ae5d917d5577d557ecb81f8f4dea3ffe7aac813e
Sandor Szabo <sanyi002@gmail.com>
2018-02-25 01:10:33 +0100

    FUTURE point counter in python

---
+++ FUTURE_counter/kard.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import fnmatch
+import re
+import time
+
+walk_dir = sys.argv[1]
+
+walk_dir = os.path.abspath(walk_dir)
+all_point = 0
+
+filename = time.strftime('%Y-%m-%d') + 'FUTURE_points_log'
+sys.stdout = open(filename, 'w')
+
+for root, subdirs, files in os.walk(walk_dir):
+    list_file_path = os.path.join(root)
+    part_point = 0
+
+    for filename in fnmatch.filter(files, '*.props'):
+        print(os.path.join(root, filename))
+        with open(os.path.join(root, filename), 'r') as props_file:
+            tmp = props_file.readlines()
+            for line in tmp: 
+                line = line.strip()           
+                part_point += int(re.sub('.*?([0-9]*)$', r'\1', line))
+            print('Részpontszám: ', part_point, '\n')
+        all_point += part_point
+
+for i in range(80):
+    print('*', end="")        
+
+print('\nÖsszes pont: ', all_point, '\n')
+
+os.close(1)
+
+
