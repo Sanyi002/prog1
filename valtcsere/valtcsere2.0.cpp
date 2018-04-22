50fd48718a739b4d2d6e1f579dae5eec54b5da2b
Sandor Szabo <sanyi002@gmail.com>
2018-04-22 20:42:56 +0200

    változók cseréje szorzással

---
+++ valtcsere/valtcsere2.0.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    cout << "a= " << a << "; b= "<< b << "\n";
+
+    a = a*b;
+    b = a/b;
+    a = a/b;
+
+    cout << "a= " << a << "; b= "<< b << "\n";
+}
